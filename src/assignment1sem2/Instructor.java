f22081271f2c3d66fc95a959430fd314080013a3
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-03-01 02:41:26 -0500

    Assignment 1 Part 2

---
+++ src/assignment1sem2/Instructor.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1sem2;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+/**
+ * this Instructor class extends from the person class
+ * @author Rutul
+ */
+
+
+public class Instructor extends Person {
+    /**
+     * Instance variable of Instructor class
+     */
+    
+    private int employeeNumber;
+    private LocalDate hireDate;
+    private ArrayList<String> courseCode = new ArrayList<>();
+    
+    /**
+     * constructor of the Instructor Class
+     * @param firstName
+     * @param lastName
+     * @param streetAddress
+     * @param city
+     * @param province
+     * @param postalCode
+     * @param birthdate
+     * @param employeeNumber
+     * @param hireDate 
+     */
+    public Instructor(String firstName, String lastName, String streetAddress, String city, String province, String postalCode, LocalDate birthdate,int employeeNumber,LocalDate hireDate) {
+        super(firstName, lastName, streetAddress, city, province, postalCode, birthdate);
+        if(employeeNumber == 0)
+            throw new IllegalArgumentException();
+        else
+            this.employeeNumber = employeeNumber;
+        setHireDate(hireDate);
+    } // end of constructor
+    
+    /**
+     * This method will return the employee number
+     * @return 
+     */
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+    
+    /**
+     * This method will return Hired Date of the instructor
+     * @return 
+     */
+    public LocalDate getHireDate() {
+        return this.hireDate;
+    }
+    
+    /**
+     * This is method of ArrayList and return the course code of the teachable courses
+     * @return 
+     */
+    public ArrayList<String> getTeachableCourses() {
+        return courseCode;
+    }
+    
+    /**
+     * This is a set method of the employee number
+     * @param employeeNumber 
+     */
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+    
+    /**
+     * This method will set and validate the Hire Date and if instructor hired mor than 80 year or Hire date is of future
+     * then it will thrown exception
+     * @param hireDate 
+     */
+    public  void setHireDate(LocalDate hireDate) {
+        if (((LocalDate.now().getYear() - hireDate.getYear()) < 80) && LocalDate.now().isAfter(hireDate))
+            this.hireDate = hireDate;                
+        else
+            throw new IllegalArgumentException("The hire date was more than 80 years ago");
+    }
+    
+    /**
+     * This method will set the teachable courses into array list
+     * @param teachableCourses 
+     */
+    public void setTeachableCourses(ArrayList<String> teachableCourses) {
+        this.courseCode = teachableCourses;
+    }
+    
+    /**
+     * this method will add the courses into the array list of courseCode 
+     * @param courseCode 
+     */
+    public void addTeachableCourse(String courseCode) {
+        this.courseCode.add(courseCode.toUpperCase());
+    }
+    
+    /**
+     * this method will validate the course is in the array list of course code
+     * @param courseCode
+     * @return 
+     */
+    public boolean canTeach(String courseCode) {
+        if(this.courseCode.contains(courseCode.toUpperCase()))
+           return true;
+        else 
+            return false;   
+    }
+    
+    /**
+     * This method will give the Total years at college from the hired date to now
+     * @return 
+     */
+    public int getYearsAtCollege() {
+        LocalDate today=LocalDate.now();        
+        int collegeYear = Period.between(hireDate,today).getYears();
+        return collegeYear;
+    }
+    
+    /**
+     * this method will return the employee number
+     * @return 
+     */
+    public int getEmployeeNum() {
+        return this.employeeNumber;
+    }
+    
+    /**
+     * This method will return the the list of subjects certified to teach
+     * @return 
+     */
+    public String listOfSubjectsCertifiedToTeach () {
+        String list = "";
+        for (int i = 0; i < courseCode.size()-1; i++) 
+            list = list + courseCode.get(i) + ", ";
+        return list + courseCode.get(courseCode.size()-1);
+    }
+    
+    /**
+     * this method will add course in the array list of the course code 
+     * @param courseCode 
+     */
+    public void addCourseToAbilities(String courseCode) {
+       this.courseCode.add(courseCode.toUpperCase());
+    }
+    
+    /**
+     * This method will validate and check the Instructor age is more than 18 and less than 100
+     * @param birthdate 
+     */
+    public void setBirthday(LocalDate birthdate) {
+        
+        if((LocalDate.now().getYear()-birthdate.getYear()) >= 18 && (LocalDate.now().getYear()-birthdate.getYear() <= 100)) 
+            this.birthdate = birthdate; 
+        else 
+            throw new IllegalArgumentException("The Instructor to be too young");
+    
+    }
+    
+}  // end of class
---
+++ src/assignment1sem2/Student.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1sem2;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Rutul
+ */
+public class Student extends Person {
+    /**
+     * instance variables
+     */
+   private LocalDate enrollDate;
+   private int studentNumber;
+   private String major;
+   private boolean isGood;
+   
+   /**
+    * constructor of student class. 
+    * @param firstName
+    * @param lastName
+    * @param streetAddress
+    * @param city
+    * @param province
+    * @param postalCode
+    * @param birthdate
+    * @param major
+    * @param studentNumber
+    * @param enrollDate 
+    */
+   
+    public Student(String firstName, String lastName, String streetAddress, String city, String province, String postalCode, LocalDate birthdate, String major, int studentNumber, LocalDate enrollDate) {
+        super(firstName, lastName, streetAddress, city, province, postalCode, birthdate);
+        setBirthday(birthdate);
+        reinstateStudent();
+        this.studentNumber = studentNumber;
+        setYearEnrolled(enrollDate);
+        setStudentNumber(studentNumber);
+    }
+    
+    /**
+     * get method of yearEnrolled 
+     * this will return enroll date year
+     * @return 
+     */
+    public int getYearEnrolled() {
+        return this.enrollDate.getYear();
+    }
+    
+    /**
+     * get method of yearsAtCollege 
+     * this will return total years at college
+     * @return 
+     */
+    public int getYearsAtCollege() {
+        return LocalDate.now().getYear()-getYearEnrolled();
+    }
+    
+    /**
+     * boolean method of inGoodStanding
+     * this will return student is  good or not
+     * @return 
+     */
+    public boolean inGoodStanding() {
+        return this.isGood; 
+    }
+    
+    /**
+     * this method will return student is reinstate or not
+     */
+    public void reinstateStudent() {
+        this.isGood = true;
+    } 
+    
+    /**
+     * setBirthday method
+     * this method will validate and return birth date of student.
+     * @param birthdate 
+     */
+    public void setBirthday(LocalDate birthdate) {
+        
+        if((LocalDate.now().getYear()-birthdate.getYear()) >= 14 && (LocalDate.now().getYear()-birthdate.getYear() <= 90)) 
+            this.birthdate = birthdate; 
+        else 
+            throw new IllegalArgumentException("The student should at least 14");
+    }
+    
+    /**
+     * this method will validate and return studentNumber 
+     * @param studentNumber 
+     */
+    public void setStudentNumber(int studentNumber) {
+      
+            if(this.studentNumber <= 0)
+            throw new IllegalArgumentException("This student number is invalid");
+        else
+            this.studentNumber = studentNumber;
+    }
+    
+    /**
+     * this method will set birth date to the instance variable from the  constructor.
+     * @param date 
+     */
+    public void setYearEnrolled(LocalDate date)
+    {
+        this.enrollDate = date;
+    }
+    
+    /**
+     * this method will set the value of  suspendedStudent.
+     */
+    public void suspendStudent() {
+        this.isGood = false;
+    } 
+   
+    
+    /**
+     * toSting of Student class
+     * @return 
+     */
+    public String toString() {     
+        return  String.format(firstName+" "+lastName+","+" student number is "+studentNumber);     
+    }
+    
+    
+} // end of class
